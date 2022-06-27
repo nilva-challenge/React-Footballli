59037dee9ea1d48da15358beb43a1483a5eddff4
ruhollahh <ruhollahh01@gmail.com>
2022-06-27 14:25:49 +0430

    install next.js and tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>React Footballli</title>
+        <meta name="description" content="Nilva Challenge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="text-red">salam</div>
+    </>
+  );
+};
+
+export default Home;
