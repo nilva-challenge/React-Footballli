50712f141dcdb28c7bb7251b05819104f374b67c
abazsh3 <abazsh3@gmail.com>
2022-07-25 12:09:39 +0430

    create topbar

--- src/containers/Fixtures/index.tsx
+++ src/containers/Fixtures/index.tsx
@@ -1,12 +1,14 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import { FixturesPageContainer } from "./styles";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
 
 export function Fixtures() {
   return (
     <FixturesPageContainer>
-      <Box>
-        <Typography variant="h4">h4. Heading</Typography>
+      <Box className="fixtures-top-bar">
+        <Typography variant="h6">نتایج زنده</Typography>
+        <AccessTimeOutlinedIcon className="topbar-icon" />
       </Box>
     </FixturesPageContainer>
   );
--- src/containers/Fixtures/styles.tsx
+++ src/containers/Fixtures/styles.tsx
@@ -1,4 +1,15 @@
 import styled from "styled-components";
 import { Box } from "@mui/material";
 
-export const FixturesPageContainer = styled(Box)``;
+export const FixturesPageContainer = styled(Box)`
+  .fixtures-top-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .topbar-icon {
+    font-size: 35px;
+    transform: scaleX(-1);
+  }
+`;
