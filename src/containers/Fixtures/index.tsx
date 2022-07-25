bddbdf998b767be79385442fecb2dfab30dc5006
abazsh3 <abazsh3@gmail.com>
2022-07-25 12:39:19 +0430

    create search input component

--- src/containers/Fixtures/index.tsx
+++ src/containers/Fixtures/index.tsx
@@ -1,23 +1,12 @@
-import { TextField } from "@mui/material";
 import { FixturesPageContainer } from "./styles";
-import SearchIcon from "@mui/icons-material/Search";
-import InputAdornment from "@mui/material/InputAdornment";
 import { Topbar } from "../../components/Topbar";
+import { SearchInput } from "../../components/SerachInput";
 
 export function Fixtures() {
   return (
     <FixturesPageContainer>
       <Topbar />
-      <TextField
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-        variant="standard"
-      />
+      <SearchInput />
     </FixturesPageContainer>
   );
 }
