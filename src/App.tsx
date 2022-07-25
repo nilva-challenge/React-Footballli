f4fcccbd0f078197d30c09a1094c70c743756fbd
abazsh3 <abazsh3@gmail.com>
2022-07-25 15:46:06 +0430

    refactor dateutils

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { AppContainer } from "./styles";
 import { Fixtures } from "./containers/Fixtures";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { theme } from "./styles/theme";
 import BottomNavigation from "./components/BottomNavigation";
-import { createDatesArray } from "./utils/dateUtils";
 
 function App() {
-  createDatesArray();
   return (
     <ThemeProvider theme={theme}>
       <AppContainer>
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,26 +1,27 @@
-import moment from "moment-jalaali";
+import moment, { max } from "moment-jalaali";
 
 //#region global
 moment.loadPersian({ dialect: "persian-modern" });
-const days = [
-  { id: -2, title: undefined },
-  { id: -1, title: "دیروز" },
-  { id: 0, title: "امروز" },
-  { id: 1, title: "فردا" },
-  { id: 2, title: undefined },
-];
+const days: { [key: string]: string } = {
+  "-1": "دیروز",
+  "0": "امروز",
+  "1": "فردا",
+};
 // #region global
 
 const formatDayTitle = (date: moment.Moment, title?: string) =>
   title || date.format("jMMMM jDD");
 
-export const createDatesArray = () => {
-  return days.map((day) => {
+export const createDatesArray = (min = -2, max = 2) => {
+  let result = [];
+  for (let ind = min; ind <= max; ind++) {
     const date = moment(new Date().toISOString(), "YYYY/M/D");
-    day.id > 0 ? date.add(day.id, "d") : date.subtract(Math.abs(day.id));
-    return {
+    ind > 0 ? date.add(ind, "d") : date.subtract(Math.abs(ind));
+    result.push({
       date,
-      title: formatDayTitle(date, day.title),
-    };
-  });
+      title: formatDayTitle(date, days[ind]),
+    });
+  }
+
+  return result;
 };
