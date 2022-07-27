47261f1fcd0950b2fdf4e9dd1749096af75d52cb
Sobhan <sobhanyazdanjo@gmail.com>
2022-07-27 16:11:47 +0430

    feat: Add top nav & search input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,33 @@
+const SearchInput = () => {
+  return (
+    <div>
+      {/* <div className="flex justify-center h-9">
+        <input type="text" placeholder="جستجو" className="bg-[#eee] min-w-[90vw] rounded" />
+      </div> */}
+      <label className="relative flex justify-center">
+        
+        <input
+          className="min-w-[90vw] bg-[#eee] placeholder:font-italitc drop-shadow-md rounded-md py-2 pl-3 pr-10 focus:outline-none"
+          placeholder="جستجو..."
+          type="text"
+        />
+
+        <button className="absolute inset-y-0 right-0 flex items-center pr-8">
+          <svg
+            className="h-5 w-5 fill-[#aaa]"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
+          >
+            <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+          </svg>
+        </button>
+      </label>
+    </div>
+  );
+};
+
+export default SearchInput;
---
+++ src/components/TopNav.tsx
@@ -0,0 +1,14 @@
+import clockImg from "../assets/images/clock.jpg";
+
+const TopNav = () => {
+  return (
+    <div className="flex justify-between p-4 pl-2">
+      <h1 className="mt-[10px] text-xl font-extrabold font-iransans">
+        نتایج زنده
+      </h1>
+      <img src={clockImg} alt="Live Score" />
+    </div>
+  );
+};
+
+export default TopNav;
