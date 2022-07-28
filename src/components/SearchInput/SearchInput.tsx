1197e1c0d42cd92cc85fa63f752f7e598a09fc31
Sobhan <sobhanyazdanjo@gmail.com>
2022-07-28 23:43:30 +0430

    refactor: Improve search input styles

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,17 +1,11 @@
 const SearchInput = () => {
   return (
     <div>
-      {/* <div className="flex justify-center h-9">
-        <input type="text" placeholder="جستجو" className="bg-[#eee] min-w-[90vw] rounded" />
-      </div> */}
-      <label className="relative flex justify-center">
-        <input
-          className="min-w-[90vw] bg-[#f5f6fb] placeholder:font-italitc drop-shadow-md rounded-md py-2 pl-3 pr-10 focus:outline-none"
-          placeholder="جستجو..."
-          type="text"
-        />
-
-        <button className="absolute inset-y-0 right-0 flex items-center pr-8">
+      <label
+        htmlFor="search"
+        className="w-11/12 m-auto my-0 bg-[#f5f6fb] h-12 rounded-md flex items-center px-4"
+      >
+        <span className="ml-2 rotate-90">
           <svg
             className="h-5 w-5 fill-[#aaa]"
             xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +17,13 @@ const SearchInput = () => {
           >
             <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
           </svg>
-        </button>
+        </span>
+        <input
+          className="bg-transparent placeholder:font-italitc focus:outline-none h-full w-full"
+          placeholder="جستجو..."
+          type="text"
+          id="search"
+        />
       </label>
     </div>
   );
