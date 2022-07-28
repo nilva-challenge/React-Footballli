8a60d235589238ae8d0c190da60e4b1c43d55ae0
Sobhan <sobhanyazdanjo@gmail.com>
2022-07-29 00:35:45 +0430

    refactor: Improve date selection implementation

--- src/components/DateSelection/DateSelection.tsx
+++ src/components/DateSelection/DateSelection.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DateSelection = () => {
+  const [date, setDate] = useState("");
   return (
-    <ul className="flex justify-center mt-6">
-      <li>دیروز</li>
-      <li className="border-b-4 border-[#5b9a3f] mx-8 rounded-b-sm pb-[5px]">
-        امروز
-      </li>
-      <li>فردا</li>
+    <ul className="flex px-4  mt-6 mb-[-10px] w-full gap-7 flex-nowrap overflow-auto">
+      {["دیروز", "امروز", "فردا"].map((item) => {
+        return (
+          <li
+            className="flex-shrink-0 cursor-pointer"
+            onClick={() => {
+              setDate(item);
+            }}
+          >
+            {item}
+
+            <span
+              className={`h-1 w-full rounded-t-md ${
+                date === item ? "bg-green-600" : ""
+              } inline-block`}
+            ></span>
+          </li>
+        );
+      })}
     </ul>
   );
 };
