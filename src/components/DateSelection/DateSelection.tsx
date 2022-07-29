71a6e0ce0a0e915bb34a4c8d31ff203ed406af9d
Sobhan <sobhanyazdanjo@gmail.com>
2022-07-29 18:13:18 +0430

    refactor: Improve style & functionality implementation of date selection

--- src/components/DateSelection/DateSelection.tsx
+++ src/components/DateSelection/DateSelection.tsx
@@ -1,22 +1,60 @@
-import React, { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import dates, { differenceInDays } from "../../utils/DateMaker";
+import { DateObject } from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
 
 const DateSelection = () => {
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+  const dateRef = useRef<null | HTMLLIElement>(null);
+  console.log("selected date is:", date);
+
+  useEffect(() => {
+    if (dateRef.current) {
+      dateRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+
+      });
+    }
+  }, []);
+
   return (
-    <ul className="flex px-4  mt-6 mb-[-10px] w-full gap-7 flex-nowrap overflow-auto">
-      {["دیروز", "امروز", "فردا"].map((item) => {
+    <ul className="flex px-4 mt-6 mb-[-10px] w-full gap-7 flex-nowrap overflow-auto">
+      {dates.map((item) => {
+        const dayInPersian = new DateObject({
+          date: item,
+          calendar: persian,
+          locale: persian_fa,
+        })
+          .format("YYYY-MM-D")
+          .split("-")[2];
+        const monthInPersian = new DateObject({
+          date: item,
+          calendar: persian,
+          locale: persian_fa,
+        }).month.name;
         return (
           <li
-            className="flex-shrink-0 cursor-pointer"
+            ref={item.split("T")[0] === date ? dateRef : null}
+            key={item.split("T")[0]}
+            className="flex-shrink-0 cursor-pointer overflow-hidden"
             onClick={() => {
-              setDate(item);
+              setDate(item.split("T")[0]);
             }}
           >
-            {item}
+            {differenceInDays(new Date(item), new Date()) === 0
+              ? "امروز"
+              : differenceInDays(new Date(item), new Date()) === 1
+              ? "فردا"
+              : differenceInDays(new Date(item), new Date()) === -1
+              ? "دیروز"
+              : `${dayInPersian} ${monthInPersian}`}
 
             <span
               className={`h-1 w-full rounded-t-md ${
-                date === item ? "bg-green-600" : ""
+                date === item.split("T")[0] ? "bg-green-600" : ""
               } inline-block`}
             ></span>
           </li>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -37,18 +37,15 @@ const Navbar = () => {
   return (
     <div className="bg-white  bg-lightMode-secondary text-darkMode-primary fixed bottom-0 flex justify-between w-full py-3 border-t">
       {navItems.reverse().map((navItem) => {
-        console.log(navItems.reverse());
         let Icon = navItem.icon;
         return (
-          <>
-            <div
-              key={navItem.name}
-              className="flex flex-col items-center flex-1 cursor-pointer text-[#acb1be] hover:text-[#5b9a3f]"
-            >
-              <Icon className="w-6 h-6" />
-              <p className=" mt-2 text-xs font-bold">{navItem.name}</p>
-            </div>
-          </>
+          <div
+            key={navItem.name}
+            className="flex flex-col items-center flex-1 cursor-pointer text-[#acb1be] hover:text-[#5b9a3f]"
+          >
+            <Icon className="w-6 h-6" />
+            <p className=" mt-2 text-xs font-bold">{navItem.name}</p>
+          </div>
         );
       })}
     </div>
