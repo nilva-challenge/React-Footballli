9fcfc01bad792e5a946976a079e6234972b2a2cc
Rezaraki <rezakadil@gmail.com>
2023-04-29 01:34:45 +0430

    add RTK Query

---
+++ src/api.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react' 
+
+
+export const pokemonApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  endpoints: (builder) => ({
+    getFixtures: builder.query<unknown, string>({
+      query: (date) =>({
+        url: 'fixtures',
+        params: { date },
+      })  ,
+    }),
+  }),
+})
+
+ 
+export const { useGetFixturesQuery } = pokemonApi
