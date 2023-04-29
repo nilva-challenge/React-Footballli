5626ddf9c59fb4f6f92795f470434989de77c55a
Rezaraki <kadkhodaei@payamava.net>
2023-04-29 17:40:00 +0330

    added tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react' 
-import './assets/styles/index.scss'
-import { DatePicker } from '../node_modules/antd/es/index'
+import './assets/styles/index.scss' 
 import Layout from './components/layout/Layout'
+import MatchPage from './components/matches/MatchPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,27 +9,10 @@ function App() {
   return (
      
     <Layout>
- 
-      <div>
-        <a href="https://vitejs.dev" target="_blank"> 
-        </a>
-        <a href="https://react.dev" target="_blank"> 
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <DatePicker />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </Layout>
+      
+      <MatchPage/>
+     
+    </Layout>
      
   )
 }
