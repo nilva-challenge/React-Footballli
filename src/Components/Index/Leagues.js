3f2a109f063045633fd9122e05fc91da6e834bb3
meysamzi <meysamzeynali2@gmail.com>
2023-04-30 16:20:42 +0430

    Date Fetched and Displayed Correctly

--- src/Components/Index/Leagues.js
+++ src/Components/Index/Leagues.js
@@ -1,9 +1,11 @@
-import { Card } from "@mui/material";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import { useState } from "react";
+import { Card, Collapse } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 
 const Leagues = ({ data }) => {
-  console.log(data);
+  const [expanded, setExpanded] = useState(true);
+
   return (
     <Card
       style={{
@@ -18,9 +20,31 @@ const Leagues = ({ data }) => {
           <img src={data?.logo} width={20} height={20} />
           <span style={{ color: "blue" }}>{data?.name}</span>
         </div>
-        <ArrowDropDownIcon />
+        <span onClick={() => setExpanded(!expanded)}>
+          {expanded ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+        </span>
       </div>
       <hr />
+      {data?.fixtures?.map((league) => {
+        let leagueTime = league?.start_time.slice(11);
+        return (
+          <Collapse in={expanded} unmountOnExit>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                gap: "0.6rem",
+              }}
+            >
+              <span>{league?.home?.name}</span>
+              <img src={league?.home?.logo} width={20} height={20} />
+              <span>{leagueTime}</span>
+              <img src={league?.away?.logo} width={20} height={20} />
+              <span>{league?.away?.name}</span>
+            </div>
+          </Collapse>
+        );
+      })}
     </Card>
   );
 };
