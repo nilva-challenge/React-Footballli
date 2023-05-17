49e07323cef5c61759a7f08d48660108acb93e0f
Farzad Abroudi <farzadabroudi@gmail.com>
2023-05-17 20:59:12 +0330

    add nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>React-Footballli</title>
+      </Head>
+      <main>
+
+      </main>
+    </>
+  )
+}
