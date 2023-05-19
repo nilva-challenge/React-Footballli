d3ab2fadc20cb2f322bbda7e3bbcdff3a8077d8d
Ali Asadollahi <ali.asadolahi74@gmail.com>
2023-05-19 16:20:51 +0330

    searchBar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "app/components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SearchBar />
     </div>
   );
 };
