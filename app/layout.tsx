23dbdb2d6365a8266e71a983903d7152b75b7c1b
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-10-24 14:42:54 +0330

    chore: add nextjs and typescript

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+const RootLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export default RootLayout;
