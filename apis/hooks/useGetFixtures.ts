dbf0b6808d6e06d5369ab8c7c0b248591d4f1f1b
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-10-26 23:52:30 +0330

    fix: await for api call

--- apis/hooks/useGetFixtures.ts
+++ apis/hooks/useGetFixtures.ts
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import { Fixtures } from '@/types';
+import axios, { AxiosResponse } from 'axios';
 import { useQuery } from 'react-query';
 
-const getFixtures = async (date: string) => {
-  return axios.get(
+const getFixtures = async (date: string): Promise<Fixtures> => {
+  const { data } = await axios.get(
     `${process.env.NEXT_PUBLIC_BASE_URL}/football/fixtures/?date=${date}`,
   );
+
+  return data;
 };
 
 const useGetFixtures = (date: string) =>
