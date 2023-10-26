d218ce4e4d2f7e24ee2cc0ef94540139a15e53af
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-10-26 10:04:54 +0330

    feat: add date bar
    * add date range
    * add functionality for scroll to today for the date range

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,79 @@
 // TODO: Create hooks for date bar
 
 import useGetFixtures from '@/apis/hooks/useGetFixtures';
+import { DateUtils } from '@/utils';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import {
+  useScroll,
+  useScrollbarWidth,
+  useScrolling,
+  useWindowScroll,
+} from 'react-use';
+
+const range = 8;
+
+const dateRanges = () => {
+  const dates = [];
+
+  for (let i = -range; i < range; i++) {
+    const today = new Date();
+    dates.push({
+      isActive: i === 0,
+      value: new Date(today.setDate(today.getDate() + i)),
+    });
+  }
+
+  return dates;
+};
+
+const handleDate = (date: Date) => {
+  const _date = new DateUtils(date);
+
+  if (_date.isYesterday()) {
+    return 'دیروز';
+  }
+
+  if (_date.isToday()) {
+    return 'امروز';
+  }
+
+  if (_date.isTomorrow()) {
+    return 'فردا';
+  }
+
+  return _date.toLocaleDateString('fa', { day: 'numeric', month: 'short' });
+};
 
 const Page = () => {
-  const date = new Date();
+  const [dates, setDates] = useState(dateRanges);
+  const dateRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (dateRef.current) {
+      dateRef.current.scrollIntoView({ inline: 'center' });
+    }
+  }, [dateRef]);
+
+  const today = new Date();
+
   const { data, isLoading } = useGetFixtures(today.toISOString().split('T')[0]);
+
+  const handleClickDate = (date: Date, index: number) => {
+    router.push('?test=test');
+
+    let _dates = [...dates.map((date) => ({ ...date, isActive: false }))];
+
+    setDates([
+      ..._dates.slice(0, index),
+      { ..._dates[index], isActive: true },
+      ..._dates.slice(index + 1),
+    ]);
+  };
+
+  return (
     <div className="pt-4 px-4 bg-white">
       <div className="flex items-center justify-between">
         <h1 className="text-lg font-semibold">نتایج زنده</h1>
@@ -37,7 +104,25 @@ const Page = () => {
         />
       </div>
 
-  return <p></p>;
+      <div className="flex gap-x-7 overflow-x-auto mt-4">
+        {dates.map((date, index) => (
+          <div
+            key={index}
+            className="relative"
+            onClick={() => handleClickDate(date.value, index)}
+            {...(date.isActive && { ref: dateRef })}
+          >
+            <div className="text-gray-400 mb-3 whitespace-nowrap">
+              {handleDate(date.value)}
+            </div>
+            {date.isActive && (
+              <div className="w-full h-1 bg-green-600 absolute bottom-0 rounded-t-lg"></div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Page;
