93b5ab474c064ba3c9f42a42c0e2bdf4b2910a5f
saeedfiroozi93 <saeedfiroozi93@gmail.com>
2023-10-26 23:48:10 +0330

    create FooterTab component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Flex } from "@mantine/core";
 import { FooterTab, LiveScoreBody, LiveScoreHeader } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <LiveScoreHeader />
-      <LiveScoreBody />
-      <FooterTab />
-    </div>
+    <>
+      <Flex direction={"column"} className="z-0">
+        <LiveScoreHeader />
+        <LiveScoreBody />
+      </Flex>
+      <div className="shadow-md shadow-black pb-[10px] fixed bottom-0 left-0 right-0 w-[100%] z-1">
+        <FooterTab />
+      </div>
+    </>
   );
 };
 export default App;
--- src/components/FooterTab.tsx
+++ src/components/FooterTab.tsx
@@ -1,6 +1,42 @@
+import { Flex, NavLink, Text } from "@mantine/core";
+import { useState } from "react";
+import { TbSoccerField } from "react-icons/tb";
+import { BsPerson } from "react-icons/bs";
+import { MdOutlineExplore } from "react-icons/md";
+import { PiSoccerBall, PiTrophyBold } from "react-icons/pi";
+
+const footerItems = [
+  { icon: TbSoccerField, label: "مسابقات" },
+  { icon: MdOutlineExplore, label: "اکتشاف" },
+  { icon: PiSoccerBall, label: "فوتبال" },
+  { icon: PiTrophyBold, label: "لیگ ها" },
+  { icon: BsPerson, label: "پروفایل" },
+];
 const FooterTab = () => {
+  const [active, setActive] = useState(0);
+
+  const items = footerItems.map((item, index) => (
+    <NavLink
+      key={item.label}
+      active={index === active}
+      label={
+        <Flex direction="column" align={"center"} gap={"10px"}>
+          <item.icon color={index !== active ? "#BDBDBD" : ""} size="2rem" />
+          <Text size={"12px"} c={index !== active ? "#BDBDBD" : ""}>
+            {item.label}
+          </Text>
+        </Flex>
+      }
+      onClick={() => setActive(index)}
+      color="#2E7D32"
+      variant="subtle"
+    />
+  ));
+
   return (
-    <div>FooterTab</div>
-  )
-}
-export default FooterTab
+    <Flex bg={"white"} direction={"row"} justify={"space-between"}>
+      {items}
+    </Flex>
+  );
+};
+export default FooterTab;
