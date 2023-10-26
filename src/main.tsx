e405b546e37c9197434dd6dce8e39c7456f17a3a
saeedfiroozi93 <saeedfiroozi93@gmail.com>
2023-10-26 23:47:52 +0330

    add Vazirmatn font

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import "./assets/fonts/Vazirmatn-font-face.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider
+      theme={{
+        fontFamily: "Vazirmatn",
+      }}
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>
