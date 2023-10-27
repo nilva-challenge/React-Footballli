c7812e0dccb21b865114a0bcf76be676e73ca487
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-10-27 18:58:38 +0330

    fix: first league shown many times

--- components/LeagueCard.tsx
+++ components/LeagueCard.tsx
@@ -30,81 +30,91 @@ const handleShowPropData = (fixture: Fixture) => {
 const LeagueCard = ({ data, search }: Props) => {
   const [accordionIsOpen, setAccordion] = useState(true);
 
+  const handleShow = () => {
+    let isShown = false;
+
+    for (let fixture of data.fixtures) {
+      if (
+        fixture.home.name.includes(search) ||
+        fixture.away.name.includes(search)
+      ) {
+        isShown = true;
+        break;
+      }
+    }
+
+    return isShown;
+  };
+
   return (
     <>
-      {data.fixtures
-        .filter(
-          (fixture) =>
-            fixture.home.name.includes(search) ||
-            fixture.away.name.includes(search),
-        )
-        .map((fixture) => (
-          <div className="bg-white rounded-lg shadow-sm" key={fixture.id}>
-            <div className="flex items-center justify-between p-4">
-              <div className="flex items-center gap-x-2">
-                <div className="w-8 h-8 rounded-full bg-gray-300"></div>
-                <div className="font-bold text-blue-500">لیگ برتر ؟</div>
-              </div>
-              <Image
-                src="/assets/icons/angle-small-down.svg"
-                width={24}
-                height={24}
-                alt="Accordion opener"
-                onClick={() => setAccordion(!accordionIsOpen)}
-                className={accordionIsOpen ? 'transform rotate-180' : ''}
-              />
+      {handleShow() && (
+        <div className="bg-white rounded-lg shadow-sm">
+          <div className="flex items-center justify-between p-4">
+            <div className="flex items-center gap-x-2">
+              <div className="w-8 h-8 rounded-full bg-gray-300"></div>
+              <div className="font-bold text-blue-500">لیگ برتر ؟</div>
             </div>
-
-            {accordionIsOpen && (
-              <>
-                <hr />
-                <div className="mt-4">
-                  {data.fixtures
-                    .filter(
-                      (fixture) =>
-                        fixture.home.name.includes(search) ||
-                        fixture.away.name.includes(search),
-                    )
-                    .map((fixture, index) => (
-                      <>
-                        {index !== 0 && <hr className="" />}
-                        <div
-                          key={fixture.id}
-                          style={{
-                            display: 'grid',
-                            gridTemplateColumns: '1fr 45px 50px 45px 1fr',
-                          }}
-                          className="items-center gap-x-2 my-4"
-                        >
-                          <div className="truncate whitespace-nowrap w-20 text-left justify-self-end">
-                            {fixture.home.name}
-                          </div>
-                          <img
-                            src={fixture.home.logo}
-                            alt=""
-                            width={45}
-                            height={45}
-                          />
-                          <div className="justify-self-center">
-                            {handleShowPropData(fixture)}
-                          </div>
-                          <img
-                            src={fixture.away.logo}
-                            alt=""
-                            width={45}
-                            height={45}
-                          />
-                          <div className="truncate whitespace-nowrap w-20">
-                            {fixture.away.name}
-                          </div>
-                        </div>
-                      </>
-                    ))}
-                </div>
-              </>
-            )}
+            <Image
+              src="/assets/icons/angle-small-down.svg"
+              width={24}
+              height={24}
+              alt="Accordion opener"
+              onClick={() => setAccordion(!accordionIsOpen)}
+              className={accordionIsOpen ? 'transform rotate-180' : ''}
+            />
           </div>
-        ))}
+
+          {accordionIsOpen && (
+            <>
+              <hr />
+              <div className="mt-4">
+                {data.fixtures
+                  .filter(
+                    (fixture) =>
+                      fixture.home.name.includes(search) ||
+                      fixture.away.name.includes(search),
+                  )
+                  .map((fixture, index) => (
+                    <>
+                      {index !== 0 && <hr className="" />}
+                      <div
+                        key={fixture.id}
+                        style={{
+                          display: 'grid',
+                          gridTemplateColumns: '1fr 45px 50px 45px 1fr',
+                        }}
+                        className="items-center gap-x-2 my-4"
+                      >
+                        <div className="truncate whitespace-nowrap w-20 text-left justify-self-end">
+                          {fixture.home.name}
+                        </div>
+                        <img
+                          src={fixture.home.logo}
+                          alt=""
+                          width={45}
+                          height={45}
+                        />
+                        <div className="justify-self-center">
+                          {handleShowPropData(fixture)}
+                        </div>
+                        <img
+                          src={fixture.away.logo}
+                          alt=""
+                          width={45}
+                          height={45}
+                        />
+                        <div className="truncate whitespace-nowrap w-20">
+                          {fixture.away.name}
+                        </div>
+                      </div>
+                    </>
+                  ))}
+              </div>
+            </>
+          )}
+        </div>
+      )}
     </>
   );
 };
