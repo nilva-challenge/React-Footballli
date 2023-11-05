bf71a64fbe23e2ead1904a1a3204c0059c9d1a16
Ali <AliNazarii7394@gmail.com>
2023-11-06 00:34:45 +0330

    Reusable Button component created

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,24 @@
+import { cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+export const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-200"],
+      dark: ["bg-secondary-dark", "hover:bg-secondary-dark-hover", "text-secondary"],
+    },
+    size: {
+      default: [" rounded", "p-2"],
+      icon: ["rounded-full", "w-10", "h-10", "flex", "items-center", "justify-center", "p-2.5"],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+export function Button({ variant, size, className, ...props }) {
+  return <button {...props} className={twMerge(buttonStyles({ variant, size }), className)} />;
+}
