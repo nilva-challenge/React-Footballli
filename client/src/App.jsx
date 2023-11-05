0664311f84eb675da760e1561cfba75e2d669b40
Ali <AliNazarii7394@gmail.com>
2023-11-06 00:11:38 +0330

    Header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Header from "./components/Header";
+import { LuClock7 as clockIcon } from "react-icons/lu";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className='flex flex-col max-w-[450px] min-w-[370px] h-[750px] rounded-3xl bg-gray-100 border-2  border-black items-center justify-center gap-3 mt-5 scrollbar-hidden relative'>
+      <Header title={"نتایج زنده"} Icon={clockIcon} />
+    </div>
+  );
+};
 
-export default App
+export default App;
