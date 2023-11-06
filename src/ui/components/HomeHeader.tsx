e84c36b8f77f3608a959369a101d2c8b47b3c406
ali <vatanparast.ali@gmail.com>
2023-11-06 08:56:56 +0330

    setup header and navigation

--- src/ui/components/HomeHeader.tsx
+++ src/ui/components/HomeHeader.tsx
@@ -1,8 +1,25 @@
+import Container from "@/ui/views/container/container";
+import {HiOutlineClock, HiOutlineSearch} from "react-icons/hi";
+
 const HomeHeader: React.FC = () => {
     return (
-        <div className='w-full bg-white h-24'>
-            HomeHeader
-        </div>
+        <Container className='w-full bg-white py-4'>
+            <div className='flex items-center justify-between'>
+                <h6>نتایج زنده</h6>
+                <HiOutlineClock/>
+            </div>
+
+            <div className="relative">
+                <div className="absolute inset-y-0 right-2 flex items-center pointer-events-none">
+                    <HiOutlineSearch/>
+                </div>
+                <input
+                    className="w-full bg-gray-300 p-2 pr-10 mt-2 focus:outline-none focus:none"
+                    type="text"
+                    placeholder="جستجو..."
+                />
+            </div>
+        </Container>
     );
 };
 
