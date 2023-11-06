e1d57262c5813df01e1d6ae81643d35f264bf3c2
ali <vatanparast.ali@gmail.com>
2023-11-06 09:08:03 +0330

    bugfix conflict files

--- src/app/components/layout.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import '@/styles/colors.css';
-
-export default function ComponentsLayout({children}: { children: React.ReactNode; }) {
-    return <>{children}</>;
-}
