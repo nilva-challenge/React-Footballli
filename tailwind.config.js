77177d40de872c147e472e4316f2aa130bb5f01c
alirezafarin <alirafarinzad@gmail.com>
2023-11-09 20:27:42 +0330

    style: added primary color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5b9a3f',
+      },
+    },
   },
   plugins: [],
 };
