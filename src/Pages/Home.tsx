a9ce14530c77b354500241356dca3535951a6138
alirezafarin <alirafarinzad@gmail.com>
2023-11-10 14:22:41 +0330

    fix: baseNavigation order changed

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,9 +5,9 @@ import { routes } from 'routes';
 
 const links: INavigationItem[] = [
   { id: 0, title: en.profile, Icon: ProfileIcon, to: routes.notFound },
-  { id: 1, title: en.navigate, Icon: NavigationIcon, to: routes.notFound },
-  { id: 2, title: en.football, Icon: FootballIcon, to: routes.notFound },
   { id: 3, title: en.leagues, Icon: TrophyIcon, to: routes.notFound },
+  { id: 2, title: en.football, Icon: FootballIcon, to: routes.notFound },
+  { id: 1, title: en.navigate, Icon: NavigationIcon, to: routes.notFound },
   { id: 4, title: en.matches, Icon: FieldIcon, to: routes.matches },
 ];
 
